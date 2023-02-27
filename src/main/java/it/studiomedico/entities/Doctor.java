e1ddd8cc76890e5012447936868c4a48ec26dc25
Greed <cristiancoltea97@gmail.com>
2023-02-27 16:27:38 +0100

    FirstCommit

---
+++ src/main/java/it/studiomedico/entities/Doctor.java
@@ -0,0 +1,4 @@
+package it.studiomedico.entities;
+
+public class Doctor {
+}
---
+++ src/test/java/it/studiomedico/StudiomedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.studiomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiomedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
