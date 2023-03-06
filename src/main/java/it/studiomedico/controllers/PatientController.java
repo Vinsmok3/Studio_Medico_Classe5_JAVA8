07c12b28a0316e95c3ecbe9ae722c40085fac6ff
Anthony Giordano <anthonygiordano1993@gmail.com>
2023-03-06 15:35:32 +0100

    Patient.

--- src/main/java/it/studiomedico/controllers/PatientController.java
+++ src/main/java/it/studiomedico/controllers/PatientController.java
@@ -1,5 +1,63 @@
 package it.studiomedico.controllers;
 
+import it.studiomedico.entities.Doctor;
+import it.studiomedico.entities.Patient;
+import it.studiomedico.repositories.PatientRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patient")
 public class PatientController {
 
+    @Autowired
+    private PatientRepository patientRepository;
+
+    //Creating patient
+    @PostMapping("/createPatient")
+    public Patient create(@RequestBody Patient patient){
+        return patientRepository.saveAndFlush(patient);
+    }
+
+    //Reading all patients
+    @GetMapping("/getAll")
+    public List<Patient> getAllPatients(){
+        return patientRepository.findAll();
+    }
+
+    // Reading a patient
+    @GetMapping("/{id}")
+    public Patient getPatient(@PathVariable long id){
+        return patientRepository.existsById(id)
+                ? patientRepository.getById(id)
+                : new Patient();
+    }
+
+    // Delete a patient
+    @DeleteMapping("/{id}")
+    public void deleteSingle(@PathVariable long id, HttpServletResponse response){
+        if (patientRepository.existsById(id))
+            patientRepository.deleteById(id);
+        else
+            response.setStatus(409);
+    }
+
+    // Delete all patients
+    @DeleteMapping("/deleteAll")
+    public void deleteAll(){
+        patientRepository.deleteAll();
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/it/studiomedico/entities/Patient.java
+++ src/main/java/it/studiomedico/entities/Patient.java
@@ -1,4 +1,122 @@
 package it.studiomedico.entities;
 
-public class Patient {
-}
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table( name = "Patient")
+
+    public class Patient {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name = "idPatient")
+        private long idPatient;
+
+        @Column(name="Name", nullable = false)
+        private String Name;
+
+        @Column(name="Surname", nullable = false)
+        private String Surname;
+
+        @Column(name="Email", nullable = false, unique = true)
+        private String Email;
+
+        @Column(name="PhoneNumber", nullable = false)
+        private String PhoneNumber;
+
+        @Column(name="Fiscal_Code", nullable = false)
+        private String Fiscal_Code;
+
+        @Column(name="Gender", nullable = false)
+        private GenderENUM Gender;
+
+
+        public long getIdPatient() {
+            return idPatient;
+        }
+
+        public void setIdPatient(long idPatient) {
+            this.idPatient = idPatient;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        public String getSurname() {
+            return Surname;
+        }
+
+        public void setSurname(String surname) {
+            Surname = surname;
+        }
+
+        public String getEmail() {
+            return Email;
+        }
+
+        public void setEmail(String email) {
+            Email = email;
+        }
+
+        public String getPhoneNumber() {
+            return PhoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            PhoneNumber = phoneNumber;
+        }
+
+        public String getFiscal_Code() {
+            return Fiscal_Code;
+        }
+
+        public void setFiscal_Code(String fiscal_Code) {
+            Fiscal_Code = fiscal_Code;
+        }
+
+        public GenderENUM getGender() {
+            return Gender;
+        }
+
+        public void setGender(GenderENUM gender) {
+            Gender = gender;
+        }
+
+
+        public Patient() {
+        }
+
+        public Patient(String name, String surname, String email, String phoneNumber, String fiscal_Code, GenderENUM gender) {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Fiscal_Code = fiscal_Code;
+            Gender = gender;
+        }
+
+        @ManyToOne
+        @JoinColumn(name = "idDoctor")
+        private Doctor doctor;
+
+        @OneToMany
+        private List<Prenotation> prenotationList;
+
+
+
+
+
+
+
+
+    }
+
+
