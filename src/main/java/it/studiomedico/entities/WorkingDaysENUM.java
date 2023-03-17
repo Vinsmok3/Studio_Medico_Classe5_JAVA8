a67a0cfe326d614983d96c2c746dce6b94ce3197
EmiTul93 <113971140+EmiTul93@users.noreply.github.com>
2023-03-17 12:14:28 +0200

    work_enum3.0

--- src/main/java/it/studiomedico/entities/WorkingDaysENUM.java
+++ src/main/java/it/studiomedico/entities/WorkingDaysENUM.java
@@ -1,21 +1,26 @@
 package it.studiomedico.entities;
 
 public enum WorkingDaysENUM {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
+    MONDAY("Monday",1),
+    TUESDAY("Tuesday",2),
+    WEDNESDAY("Wednesday",3),
+    THURSDAY("Thursday",4),
+    FRIDAY("Friday",5),
+    SATURDAY("Saturday",6),
+    SUNDAY("Sunday",7);
+
+   private String days;
+   private int id;
+
+    WorkingDaysENUM(String days, int id) {
+        this.days = days;
+        this.id = id;
+    }
 
     public void workingDays() {
         for (WorkingDaysENUM days : WorkingDaysENUM.values()) {
             System.out.println(days.name());
-
         }
-
-
     }
 }
 
